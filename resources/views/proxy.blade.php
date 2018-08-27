ef4d0ee6eec654ed0e14b1c6249b70b89472ef89
Aman <amanbek_94@mail.ru>
2018-08-27 11:09:11 +0600

    err

--- resources/views/proxy.blade.php
+++ resources/views/proxy.blade.php
@@ -4,16 +4,6 @@
 <div class="container">
   <div class="flex-center position-ref full-height">
     <div class="row col-md-12" style="color: #777">
-      <?php
-       error_reporting(E_ALL);
-       $ch = curl_init();
-       curl_setopt($ch, CURLOPT_URL, "http://2ip.ru/");
-       curl_setopt($ch, CURLOPT_PROXY, '127.0.0.1:9050');
-       curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-       $result = curl_exec($ch);
-       curl_close($ch);
-       echo $result;
-      ?>
       OK: https://tengrinews.kz/ NOT: https://whoer.net/ru
       <div style="padding:5px;">
       <input type="text" id="url_name" value="">
